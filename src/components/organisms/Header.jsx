124ed6616e7df4b98e74f91e634890e0900ca35d
unknown <danirizky704@gmail.com>
2023-05-20 17:12:26 +0700

    feat: add shoping bag header

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import CartItem from "../templates/CartItem";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <div>Header</div>
+      <Link to={`/CartItem`} className="h-10 w-12">
+      <FontAwesomeIcon icon={faBagShopping} size="lg" />
+      </Link>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
