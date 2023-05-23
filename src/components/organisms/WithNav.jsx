0bff9c392da88ea18949428e05ac938fcb13ec1e
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-05-23 09:56:43 +0700

    feat: hide header, sidebar, and footer in login page

---
+++ src/components/organisms/WithNav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+import Header from './Header'
+import Sidebar from './Sidebar'
+import Footer from './Footer'
+
+const WithNav = () => {
+  return (
+    <>
+        <Header />
+        <Outlet />
+        <Footer />
+        <Sidebar />
+        <Footer />
+    </>
+  )
+}
+
+export default WithNav
