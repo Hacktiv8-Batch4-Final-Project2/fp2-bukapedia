496956b189be82120c012968bc1042113c05d86a
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-05-25 00:33:25 +0700

    feat: add logic hiden if qty < 1

--- src/components/templates/Product.jsx
+++ src/components/templates/Product.jsx
@@ -16,57 +16,59 @@ const Product = ({ product }) => {
 
   const user = JSON.parse(localStorage.getItem("user"));
   const { id, title, price, image, category, description, rating } = product;
-  return (
-    <div>
-      <div
-        className="border border-[#e4e4e4] h-[300px] mb-4 relative 
-    overflow-hidden group transition"
-      >
-        <div className="w-full h-full flex justify-center items-center">
-          {/* image */}
-          <div className="w-[200px] mx-auto flex justify-center items-center">
-            <img
-              className="max-h-[160px] group-hover:scale-110 transition duration-300"
-              src={image}
-              alt={title}
-            />
+  if (product.qty > 0) {
+    return (
+      <div>
+        <div
+          className="border border-[#e4e4e4] h-[300px] mb-4 relative 
+      overflow-hidden group transition"
+        >
+          <div className="w-full h-full flex justify-center items-center">
+            {/* image */}
+            <div className="w-[200px] mx-auto flex justify-center items-center">
+              <img
+                className="max-h-[160px] group-hover:scale-110 transition duration-300"
+                src={image}
+                alt={title}
+              />
+            </div>
+          </div>
+          {/* buttons */}
+          <div className="absolute top-2 right-2 group-hover:right-5 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all">
+            <button onClick={() => {
+              if (!user.token) {
+                navigate("/login")
+              }
+              if (product.qty < 1) {
+                alert('Stok Habis')
+              }
+              dispatch(addToCart(product))
+            }} >
+              <div
+                className="flex justify-center items-center
+              text-white w-10 h-10 bg-green-500"
+              >
+                <FontAwesomeIcon icon={faPlus} />
+              </div>
+            </button>
+            <Link
+              to={`/product/${id}`}
+              className="w-10 h-10 bg-white flex justify-center items-center text-primary drop-shadow-xl"
+            >
+              <FontAwesomeIcon icon={faEye} />
+            </Link>
           </div>
         </div>
-        {/* buttons */}
-        <div className="absolute top-2 right-2 group-hover:right-5 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all">
-          <button onClick={() => {
-            if (!user.token) {
-              navigate("/login")
-            }
-            if (product.qty < 1) {
-              alert('Stok Habis')
-            }
-            dispatch(addToCart(product))
-          }} >
-            <div
-              className="flex justify-center items-center
-            text-white w-10 h-10 bg-green-500"
-            >
-              <FontAwesomeIcon icon={faPlus} />
-            </div>
-          </button>
-          <Link
-            to={`/product/${id}`}
-            className="w-10 h-10 bg-white flex justify-center items-center text-primary drop-shadow-xl"
-          >
-            <FontAwesomeIcon icon={faEye} />
+        <div>
+          <div className="text-sm capitalize text-gray-500 mb-1 ">{category}</div>
+          <Link to={`/product/${id}`}>
+            <h2 className="font-semibold mb-1">{title}</h2>
           </Link>
+          <div className="font-semibold">$ {price}</div>
         </div>
       </div>
-      <div>
-        <div className="text-sm capitalize text-gray-500 mb-1 ">{category}</div>
-        <Link to={`/product/${id}`}>
-          <h2 className="font-semibold mb-1">{title}</h2>
-        </Link>
-        <div className="font-semibold">$ {price}</div>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Product;
